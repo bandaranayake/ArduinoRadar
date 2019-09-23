947d4d03b68280d7d5096c1fb4e385da0b10d5eb
Isuru Bandaranayake <isbana.git@gmail.com>
2019-09-24 00:30:28 +0530

    Changed directory

---
+++ Arduino/Radar.ino
@@ -0,0 +1,43 @@
+#include <Servo.h> 
+#define trigPin 12
+#define echoPin 11
+#define servoPin 10
+
+Servo servo; 
+
+void setup() { 
+  servo.attach(servoPin); 
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop(){
+  for(int servoAngle = 0; servoAngle <= 180; servoAngle++)
+  {                                  
+    servo.write(servoAngle);              
+    delay(50);
+    print(servoAngle);       
+  }
+  
+  for(int servoAngle = 180; servoAngle >= 0; servoAngle--)
+  {                                  
+    servo.write(servoAngle);              
+    delay(50);
+    print(0-servoAngle);       
+  }
+}
+
+void print(int servoAngle){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  int duration = pulseIn(echoPin, HIGH);
+  int cm = (duration/2) / 29.1;
+  Serial.print(servoAngle);
+  Serial.print(","); 
+  Serial.println(cm);  
+}
